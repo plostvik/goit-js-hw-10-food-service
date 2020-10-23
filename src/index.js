ea5252f790950df9f020e7c25a7b5f40edfc1fb0
Ostap Pluzhnik <plostvik@gmail.com>
2020-10-23 14:49:13 +0300

    Добавил отдельный шаблон и подключил его

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles.css';
 import menu from './menu.json';
-import Handlebars from 'handlebars';
+import sourceTemplate from "./templates/menuTemplate.hbs"
 
 // Тема
 // Добавь функционал изменения темы при нажатии (событие change) на чекбокс #theme-switch-toggle в тулбаре.
@@ -28,6 +28,15 @@ const body = document.querySelector('body');
 //     body.classList.add('light-theme');
 //   }
 // }
+// toolbar.addEventListener('click', () => {
+//   if (toolbar.checked) {
+//     localStorage.setItem('theme', Theme.DARK);
+//     body.classList.replace('light-theme', 'dark-theme');
+//   } else {
+//     localStorage.setItem('theme', Theme.LIGHT);
+//     body.classList.replace('dark-theme', 'light-theme');
+//   }
+// });
 
 // ________________КОРОТКИЙ ВАРИАНТ
 const currentTheme = localStorage.getItem('theme') || Theme.LIGHT;
@@ -44,49 +53,12 @@ toolbar.addEventListener('click', () => {
   }
 });
 
-toolbar.addEventListener('click', () => {
-  if (toolbar.checked) {
-    localStorage.setItem('theme', Theme.DARK);
-    body.classList.replace('light-theme', 'dark-theme');
-  } else {
-    localStorage.setItem('theme', Theme.LIGHT);
-    body.classList.replace('dark-theme', 'light-theme');
-  }
-});
-
 // Шаблонизация
 // Используя шаблонизатор Handlebars создай шаблон одного элемента меню. После чего, используя шаблон, создай разметку всего меню по данным из menu.json и добавь в DOM в ul.js-menu.
 // Для иконок используется библиотека Material Icons, линк на веб-фонт уже есть в исходном HTML.
 // Ниже указана разметка элемента меню которая должна получаться по шаблону (данные будут разные для каждого объекта).
 
-const sourceTemplate = ` <li class="menu__item">
-        <article class="card">
-          <img src={{ image }} alt={{ name }} class="card__image" />
-          <div class="card__content">
-            <h2 class="card__name">{{ name }}</h2>
-            <p class="card__price">
-              <i class="material-icons"> monetization_on </i>
-              {{ price }} кредитов
-            </p>
-
-            <p class="card__descr">{{ description }}</p>
-
-            <ul class="tag-list">
-              {{#each ingredients}}
-              <li class="tag-list__item">{{this}}</li>
-              {{/each}}
-            </ul>
-          </div>
-          <button class="card__button button">
-            <i class="material-icons button__icon"> shopping_cart </i>В корзину
-          </button>
-        </article>
-      </li>
-    `
-
 const menuList = document.querySelector(".js-menu") 
-const compiler = Handlebars.compile(sourceTemplate);
-console.log(compiler)
 menu.forEach(el => {
-  menuList.innerHTML += compiler(el)
+  menuList.innerHTML += sourceTemplate(el)
 })
